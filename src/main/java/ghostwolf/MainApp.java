fce473653f8f59ff1e1e128c2110744875c900c9
wushubin <ghost1874@qq.com>
2017-06-29 11:12:38 +0800

    测试

--- src/main/java/ghostwolf/MainApp.java
+++ src/main/java/ghostwolf/MainApp.java
@@ -9,7 +9,7 @@ public class MainApp {
      * A main() so we can easily run these routing rules in our IDE
      */
     public static void main(String... args) throws Exception {
-
+        System.out.println("test ....   ");
     }
 
 }
