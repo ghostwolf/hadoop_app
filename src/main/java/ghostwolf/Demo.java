fcbef940d2e7c1f7945aa9aab64739d757155706
wushubin <ghost1874@qq.com>
2017-06-29 11:20:53 +0800

    测试

--- src/main/java/ghostwolf/Demo.java
+++ src/main/java/ghostwolf/Demo.java
@@ -5,6 +5,8 @@ package ghostwolf;
  */
 public class Demo {
     public static void main(String... args) throws Exception {
-        System.out.println("test ....   ");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
     }
 }
